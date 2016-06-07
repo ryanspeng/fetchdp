2f0bf332865ee9ae205b08f57935375a7b34ae6c
ryanspeng <songpeng11@gmail.com>
2016-06-07 16:52:55 +0800

    generate ids

---
+++ gen_ids.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import json
+
+ids = set([])
+f = file('shop_ids', 'w')
+for l in file('shops'):
+    x = json.loads(l.strip())
+    if x['id'] in ids:
+        print "Dup:", x['id']
+    else:
+        ids.add(x['id'])
+        print >>f, x['id']
+f.close()
